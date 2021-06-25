b59693bd2b08d2b512d4d44137251149075a2908
Timothe-Renaud <timothe8833@gmail.com>
2021-06-25 08:56:32 +0200

    push du script d'installa express de docker + pul des images necessaire pour graylog conteneurisé

--- install.sh
+++ install.sh
@@ -6,6 +6,23 @@
 #
 #   commande officiel tirer sur site Graylog.org
 #
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release -y
+
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+cho \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 sudo docker run --name mongo -d mongo:4.2
 
